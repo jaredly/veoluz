e48fd7e912643dbb5679ff64b00340f690b5a194
Jared Forsyth <jared@jaredforsyth.com>
2019-05-31 23:02:56 -0600

    some more unrolling

--- src/line.rs
+++ src/line.rs
@@ -2,6 +2,108 @@ extern crate num_traits;
 use num_traits::{Float, NumAssignOps, NumCast, Signed};
 use std::mem::swap;
 
+pub fn draw_line(
+    mut start: Point<f32>,
+    mut end: Point<f32>,
+    data: &mut [usize],
+    width: usize,
+    height: usize,
+    full: f32,
+) {
+    // -- The Setup part
+
+    let steep = (end.1 - start.1).abs() > (end.0 - start.0).abs();
+
+    if steep {
+        start = (start.1, start.0);
+        end = (end.1, end.0);
+    }
+
+    if start.0 > end.0 {
+        swap(&mut start, &mut end);
+    }
+    // log!("Line xs = {} - {}", start.0, end.0);
+
+    let mut gradient = (end.1 - start.1) / (end.0 - start.0);
+
+    if gradient == 0.0 {
+        gradient = 1.0;
+    }
+
+    let mut x = start.0.round() as i16;
+    let mut y = start.1;
+    let end_x = end.0.round() as i16;
+    let mut lower = false;
+
+    // -- the algorithm
+
+    #[inline]
+    fn draw(data: &mut [usize], width: usize, full: f32, x: i16, y: i16, amount: f32) {
+        let index = (y as usize) * width + x as usize;
+        let brightness = (amount * full) as usize;
+        data[index] += brightness;
+    }
+
+    for x in start.0.round() as i16..end.0.round() as i16 {
+        let fpart = y.fract();
+        let mut yy = y as i16;
+
+        // Get the point
+        let point = if steep { (yy, x) } else { (x, yy) };
+
+        if point.0 >= 0 && point.1 >= 0 && point.0 < width as i16 && point.1 < height as i16 {
+            draw(data, width, full, point.0, point.1, 1.0 - fpart)
+        }
+
+        if fpart > 0.0 {
+            let point = if steep { (yy + 1, x) } else { (x, yy + 1) };
+            if point.0 >= 0 && point.1 >= 0 && point.0 < width as i16 && point.1 < height as i16 {
+                draw(data, width, full, point.0, point.1, fpart)
+            }
+        }
+        y += gradient;
+    }
+
+    // backing out this loop took us from 322 - 302 for 50k
+    // loop {
+    //     if x <= end_x {
+    //         let fpart = y.fract();
+    //         let mut yy = y as i16;
+    //         if lower {
+    //             yy += 1;
+    //         }
+
+    //         // Get the point
+    //         let point = if steep { (yy, x) } else { (x, yy) };
+
+    //         if point.0 >= 0 && point.1 >= 0 && point.0 < width as i16 && point.1 < height as i16 {
+    //             draw(data, width, full, point.0, point.1, if lower { fpart } else { 1.0 - fpart })
+    //         }
+
+    //         if lower {
+    //             lower = false;
+    //             x += 1;
+    //             y += gradient;
+    //         } else {
+    //             if fpart > 0.0 {
+    //                 lower = true;
+    //             } else {
+    //                 x += 1;
+    //                 y += gradient;
+    //             }
+    //         }
+    //     } else {
+    //         break;
+    //     };
+
+    //     // if x >= 0 && y >= 0 && x < width as i16 && y < height as i16 {
+    //     //     draw(data, width, full, x, y, amount)
+    //     // }
+    // }
+}
+
+
+
 /// An implementation of [Xiaolin Wu's line algorithm].
 ///
 /// This algorithm works based on floating-points and returns an extra variable for how much a
@@ -74,87 +176,6 @@ impl XiaolinWu<f32, i16> {
             end_x: end.0.round() as i16,
             lower: false,
         }
-    }
-}
-
-pub fn draw_line(
-    mut start: Point<f32>,
-    mut end: Point<f32>,
-    data: &mut [usize],
-    width: usize,
-    height: usize,
-    full: f32,
-) {
-    // -- The Setup part 
-
-    let steep = (end.1 - start.1).abs() > (end.0 - start.0).abs();
-
-    if steep {
-        start = (start.1, start.0);
-        end = (end.1, end.0);
-    }
-
-    if start.0 > end.0 {
-        swap(&mut start, &mut end);
-    }
-    // log!("Line xs = {} - {}", start.0, end.0);
-
-    let mut gradient = (end.1 - start.1) / (end.0 - start.0);
-
-    if gradient == 0.0 {
-        gradient = 1.0;
-    }
-
-    let mut x = start.0.round() as i16;
-    let mut y = start.1;
-    let mut end_x = end.0.round() as i16;
-    let mut lower = false;
-
-    // -- the algorithm
-
-    loop {
-        let ((x, y), amount) = if x <= end_x {
-            // get the fractional part of y
-            let fpart = y - y.floor();
-
-            // Calculate the integer value of y
-            let mut yy = NumCast::from(y).unwrap();
-            if lower {
-                yy += 1;
-            }
-
-            // Get the point
-            let point = if steep { (yy, x) } else { (x, yy) };
-
-            if lower {
-                // Return the lower point
-                lower = false;
-                x += 1;
-                y += gradient;
-                (point, fpart)
-            } else {
-                if fpart > 0.0 {
-                    // Set to return the lower point if the fractional part is > 0
-                    lower = true;
-                } else {
-                    // Otherwise move on
-                    x += 1;
-                    y += gradient;
-                }
-
-                // Return the remainer of the fractional part
-                (point, 1.0 - fpart)
-            }
-        } else {
-            // log!("Bailing {}, {}", x, end_x);
-            break;
-        };
-        if x < 0 || y < 0 || x >= width as i16 || y >= height as i16 {
-            continue;
-        }
-        let index = (y as usize) * width + x as usize;
-        let brightness = (amount * full) as usize;
-        data[index] += brightness;
     }
 }
 
