7bc6ae03086cf2b1577ad7864282f4e1e1766bd7
Jared Forsyth <jared@jaredforsyth.com>
2019-06-04 08:45:32 -0600

    fmt

--- src/lib.rs
+++ src/lib.rs
@@ -19,12 +19,12 @@ macro_rules! log {
     }
 }
 
-use nalgebra::geometry::{Isometry2, Translation2, Rotation2};
 use nalgebra as na;
+use nalgebra::geometry::{Isometry2, Rotation2, Translation2};
 
 enum BallResult {
     Inside(line::float),
-    Outside(line::float, line::float)
+    Outside(line::float, line::float),
 }
 
 #[inline]
@@ -32,15 +32,14 @@ fn ball_toi_with_ray(
     center: &Point2<line::float>,
     radius: line::float,
     ray: &Ray<line::float>,
-) -> Option<BallResult>
-{
+) -> Option<BallResult> {
     let dcenter = ray.origin - *center;
 
     // this confuses me, because I think the radius's dir is supposed to be noramlized, e.g. of length one
     let a = ray.dir.norm_squared();
     // the vector from the circle to the start of the ray, projected onto the line of the ray
     // if this is negative, then the ray has to start within the circle in order to intersect
-    // if it's positive, then the tangent of the 
+    // if it's positive, then the tangent of the
     let b = dcenter.dot(&ray.dir);
     // the (distance from the ray start to the circle center)^2 - radius^2
     // if this is positive, the ray starts outside of the circle. if negative, it starts inside
@@ -73,7 +72,7 @@ use ncollide2d::query::RayIntersection;
 #[inline]
 fn is_between(needle: line::float, start: line::float, end: line::float) -> bool {
     if start == -std::f32::consts::PI && end == std::f32::consts::PI {
-        return true
+        return true;
     }
     // TODO remove these
     let needle = angle_norm(needle);
@@ -87,7 +86,15 @@ fn is_between(needle: line::float, start: line::float, end: line::float) -> bool
 }
 
 #[inline]
-fn ray_arc_collision(ray: &Ray<line::float>, arc: (&Ball<line::float>, &Point2<line::float>, line::float, line::float)) -> Option<ncollide2d::query::RayIntersection<line::float>> {
+fn ray_arc_collision(
+    ray: &Ray<line::float>,
+    arc: (
+        &Ball<line::float>,
+        &Point2<line::float>,
+        line::float,
+        line::float,
+    ),
+) -> Option<ncollide2d::query::RayIntersection<line::float>> {
     match ball_toi_with_ray(&arc.1, arc.0.radius(), &ray) {
         None => None,
         Some(BallResult::Inside(dist)) => {
@@ -96,33 +103,41 @@ fn ray_arc_collision(ray: &Ray<line::float>, arc: (&Ball<line::float>, &Point2<l
 
             let place = normal.y.atan2(normal.x) + std::f32::consts::PI;
             if is_between(place, arc.2, arc.3) {
-                Some(RayIntersection::new(dist, normal, ncollide2d::shape::FeatureId::Face(0)))
+                Some(RayIntersection::new(
+                    dist,
+                    normal,
+                    ncollide2d::shape::FeatureId::Face(0),
+                ))
             } else {
                 None
             }
         }
         Some(BallResult::Outside(closer, farther)) => {
-
             let pos = ray.origin + ray.dir * closer - arc.1;
             let normal = -pos.normalize();
 
             let place = normal.y.atan2(normal.x) + std::f32::consts::PI;
             if is_between(place, arc.2, arc.3) {
-                Some(RayIntersection::new(closer, normal, ncollide2d::shape::FeatureId::Face(0)))
+                Some(RayIntersection::new(
+                    closer,
+                    normal,
+                    ncollide2d::shape::FeatureId::Face(0),
+                ))
             } else {
-
                 let pos = ray.origin + ray.dir * farther - arc.1;
                 let normal = -pos.normalize();
 
                 let place = normal.y.atan2(normal.x) + std::f32::consts::PI;
                 if is_between(place, arc.2, arc.3) {
-                    Some(RayIntersection::new(farther, normal, ncollide2d::shape::FeatureId::Face(0)))
+                    Some(RayIntersection::new(
+                        farther,
+                        normal,
+                        ncollide2d::shape::FeatureId::Face(0),
+                    ))
                 } else {
                     None
                 }
-
             }
-
         }
     }
 }
@@ -132,8 +147,8 @@ pub fn draw(
     ctx: &CanvasRenderingContext2d,
     width: u32,
     height: u32,
-    real: f64,
-    imaginary: f64,
+    _real: f64,
+    _imaginary: f64,
 ) -> Result<(), JsValue> {
     let _timer = Timer::new("draw all");
 
@@ -145,37 +160,50 @@ pub fn draw(
         // ncollide2d::shape::Segment::new(Point2::new(550.0, 100.0), Point2::new(551.0, 500.0)),
         // ncollide2d::shape::Segment::new(Point2::new(100.0, 100.0), Point2::new(350.0, 101.0)),
         // ncollide2d::shape::Segment::new(Point2::new(100.0, 550.0), Point2::new(500.0, 561.0)),
-        Wall::Circle(
-            Ball::new(50.0),
-            Point2::new(cx, cy - 150.0),
-            -std::f32::consts::PI,
-            std::f32::consts::PI
-        ),
-        Wall::Circle(
-            Ball::new(50.0),
-            Point2::new(cx + 250.0, cy),
-            -std::f32::consts::PI / 2.0,
-            std::f32::consts::PI / 2.0
-        ),
-        Wall::Circle(
-            Ball::new(50.0),
-            Point2::new(cx, cy + 250.0),
-            0.0,
-            std::f32::consts::PI
-        ),
+
+        // Wall::Circle(
+        //     Ball::new(50.0),
+        //     Point2::new(cx, cy - 150.0),
+        //     -std::f32::consts::PI,
+        //     std::f32::consts::PI
+        // ),
+        // Wall::Circle(
+        //     Ball::new(50.0),
+        //     Point2::new(cx + 250.0, cy),
+        //     -std::f32::consts::PI / 2.0,
+        //     std::f32::consts::PI / 2.0
+        // ),
+        // Wall::Circle(
+        //     Ball::new(50.0),
+        //     Point2::new(cx, cy + 250.0),
+        //     0.0,
+        //     std::f32::consts::PI
+        // ),
+
     ];
 
     let count = 5;
 
     for i in 0..count {
         let theta = i as line::float / (count as line::float) * line::PI * 2.0;
-        let r0 = 50.0;
-        let r1 = 200.0;
-        let td = line::PI / (count as line::float / 2.0) + i as line::float * 0.1;
-        walls.push(Wall::Line(ncollide2d::shape::Segment::new(
+        let r0 = 300.0;
+        let r1 = 350.0;
+        let td = line::PI / (count as line::float * 4.0) + i as line::float * 0.1;
+
+        walls.push(WallType::Line(ncollide2d::shape::Segment::new(
             Point2::new(cx + theta.cos() * r0, cy + theta.sin() * r0),
             Point2::new(cx + (theta + td).cos() * r1, cy + (theta + td).sin() * r1),
-        )))
+        )));
+
+        walls.push(WallType::Circle(
+            Ball::new(200.0),
+            Point2::new(
+                cx + (theta + td).cos() * 200.0,
+                cy + (theta + td).sin() * 200.0,
+            ),
+            angle_norm(theta - std::f32::consts::PI * 4.0 / 5.0),
+            angle_norm(theta - std::f32::consts::PI * 3.0 / 5.0),
+        ))
     }
 
     let mut data = zen_photon(&walls, width as usize, height as usize);
@@ -184,9 +212,9 @@ pub fn draw(
     ctx.put_image_data(&data, 0.0, 0.0)?;
 
     ctx.set_stroke_style(&JsValue::from_str("green"));
-    for wall in walls.iter() {
-        wall.draw(&ctx);
-    }
+    // for wall in walls.iter() {
+    //     wall.draw(&ctx);
+    // }
 
     Ok(())
 }
@@ -231,10 +259,10 @@ fn bounce_ray(
     // pass through
     // } else
     if r < 0.1 {
-        (ray.point_at(toi + 1.0), false)
+        (ray.point_at(toi + 0.1), false)
     // reflect
     } else {
-        let new_origin = ray.point_at(toi - 1.0);
+        let new_origin = ray.point_at(toi - 0.1);
         let ray_dir = ray.dir.y.atan2(ray.dir.x);
         let normal_dir = normal.y.atan2(normal.x) + 3.14159 / 2.0;
 
@@ -248,50 +276,48 @@ fn bounce_ray(
 
 use ncollide2d::shape::Segment;
 
-enum Wall {
-    Line(Segment<line::float>),
-    Circle(ncollide2d::shape::Ball<line::float>, Point2<line::float>, line::float, line::float)
+struct Wall {
+    kind: WallType,
+    reflect: f32,
+    transmit: f32,
+    refraction: f32,
 }
 
-impl Wall {
-    fn toi_and_normal_with_ray(&self, ray: &Ray<line::float>) -> Option<ncollide2d::query::RayIntersection<line::float>> {
+enum WallType {
+    Line(Segment<line::float>),
+    Circle(
+        ncollide2d::shape::Ball<line::float>,
+        Point2<line::float>,
+        line::float,
+        line::float,
+    ),
+}
+
+impl WallType {
+    fn toi_and_normal_with_ray(
+        &self,
+        ray: &Ray<line::float>,
+    ) -> Option<ncollide2d::query::RayIntersection<line::float>> {
         use ncollide2d::query::ray_internal::ray::RayCast;
         match self {
-            Wall::Line(wall) => wall.toi_and_normal_with_ray(&nalgebra::geometry::Isometry::identity(), ray, true),
-            Wall::Circle(circle, center, t0, t1) => {
+            WallType::Line(wall) => {
+                wall.toi_and_normal_with_ray(&nalgebra::geometry::Isometry::identity(), ray, true)
+            }
+            WallType::Circle(circle, center, t0, t1) => {
                 ray_arc_collision(&ray, (circle, center, *t0, *t1))
-
-                // if *t0 == -std::f32::consts::PI && *t1 == std::f32::consts::PI {
-                //     res
-                // } else {
-                //     match res {
-                //         None => None,
-                //         Some(contact) => {
-                //             // could probably do some heuristics with quadrants to avoid this some times
-                //             let normal = contact.normal.y.atan2(contact.normal.x);
-                //             if normal > *t0 && normal < *t1 {
-                //                 Some(contact)
-                //             } else {
-                //                 None
-                //             }
-                //         }
-                //     }
-                // }
-
-            } 
+            }
         }
     }
 
     fn draw(&self, ctx: &CanvasRenderingContext2d) {
         match self {
-            Wall::Line(wall) => {
-
+            WallType::Line(wall) => {
                 ctx.begin_path();
-        ctx.move_to(wall.a().x as f64, wall.a().y as f64);
-        ctx.line_to(wall.b().x as f64, wall.b().y as f64);
-        ctx.stroke();
+                ctx.move_to(wall.a().x as f64, wall.a().y as f64);
+                ctx.line_to(wall.b().x as f64, wall.b().y as f64);
+                ctx.stroke();
             }
-            Wall::Circle(circle, center, t0, t1) => {
+            WallType::Circle(circle, center, t0, t1) => {
                 ctx.begin_path();
                 ctx.ellipse(
                     center.x as f64,
@@ -300,7 +326,7 @@ impl Wall {
                     circle.radius() as f64,
                     0.0,
                     *t0 as f64,
-                    *t1 as f64
+                    *t1 as f64,
                 );
                 ctx.stroke();
             }
@@ -308,7 +334,10 @@ impl Wall {
     }
 }
 
-fn find_collision(walls: &[Wall], ray: &Ray<line::float>) -> Option<(line::float, usize, Vector2<line::float>)> {
+fn find_collision(
+    walls: &[WallType],
+    ray: &Ray<line::float>,
+) -> Option<(line::float, usize, Vector2<line::float>)> {
     let mut closest = None;
 
     for (i, wall) in walls.iter().enumerate() {
@@ -324,7 +353,7 @@ fn find_collision(walls: &[Wall], ray: &Ray<line::float>) -> Option<(line::float
     closest
 }
 
-fn zen_photon(walls: &[Wall], width: usize, height: usize) -> Vec<u8> {
+fn zen_photon(walls: &[WallType], width: usize, height: usize) -> Vec<u8> {
     let _timer = Timer::new("Calculate");
 
     let mut brightness_data = vec![0; width * height];
@@ -334,19 +363,34 @@ fn zen_photon(walls: &[Wall], width: usize, height: usize) -> Vec<u8> {
 
     for _ in 0..100_000 {
         let direction = random::<line::float>() * 3.14159 * 2.0;
-        let mut ray = ncollide2d::query::Ray::new(point, Vector2::new(direction.cos(), direction.sin()));
+        let mut ray =
+            ncollide2d::query::Ray::new(point, Vector2::new(direction.cos(), direction.sin()));
         let max_brightness = 5.0;
 
         for _ in 0..30 {
             match find_collision(walls, &ray) {
-            // match None {
+                // match None {
                 None => {
-                    line::draw_line(xy(&ray.origin), xy(&ray.point_at(600.0)), &mut brightness_data, width, height, max_brightness);
+                    line::draw_line(
+                        xy(&ray.origin),
+                        xy(&ray.point_at(600.0)),
+                        &mut brightness_data,
+                        width,
+                        height,
+                        max_brightness,
+                    );
                     break;
                 }
                 Some((toi, wall_index, normal)) => {
                     let (new_origin, stop) = bounce_ray(&mut ray, toi, wall_index, normal);
-                    line::draw_line(xy(&ray.origin), xy(&new_origin), &mut brightness_data, width, height, max_brightness);
+                    line::draw_line(
+                        xy(&ray.origin),
+                        xy(&new_origin),
+                        &mut brightness_data,
+                        width,
+                        height,
+                        max_brightness,
+                    );
                     ray.origin = new_origin;
                     if stop {
                         break;
@@ -368,7 +412,7 @@ fn zen_photon(walls: &[Wall], width: usize, height: usize) -> Vec<u8> {
 
     let mut data = vec![0; width * height * 4];
     let top = top as line::float;
-    // let scale = 
+    // let scale =
     for x in 0..width {
         for y in 0..height {
             let index = (x + y * width) * 4;
